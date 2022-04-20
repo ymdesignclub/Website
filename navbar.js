2a967859bfcd7e74fef141725df48f87074c9ab5
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-20 11:07:05 -0400

    Clean up navbar.js

--- navbar.js
+++ navbar.js
@@ -1,12 +1,11 @@
-window.onscroll = function() {myFunction()};
-
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
+
+window.onscroll = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
-}
+};
