c285ce0fb1189a8c78b95e9bc43ae552eb072bf3
Bryan Yan <tyrone.yan@hotmail.com>
2022-04-20 11:07:21 -0400

    Add semicolon

--- navbar.js
+++ navbar.js
@@ -4,7 +4,7 @@ var sticky = navbar.offsetTop;
 
 window.onscroll = () => {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
